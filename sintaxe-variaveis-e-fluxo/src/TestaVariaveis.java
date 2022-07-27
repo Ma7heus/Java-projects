2783b4fe728cd80f4350a4d5dac85ddade3b7ca5
MATHEUS HENRIQUE SALDIAS PALACIOS PIETRO BIAS <86833526+Ma7heus@users.noreply.github.com>
2022-07-27 15:12:02 -0300

    Trabalhando com variaveis

---
+++ sintaxe-variaveis-e-fluxo/src/TestaVariaveis.java
@@ -0,0 +1,14 @@
+
+public class TestaVariaveis {
+	public static void main(String[] args) {
+		System.out.println("novo teste");
+		
+		// Para de clarar uma variavekl e necessario informat o tipo
+		int idade;
+		idade = 21;
+		System.out.println("Minha idade e " + idade);
+		
+		
+	}
+
+}
