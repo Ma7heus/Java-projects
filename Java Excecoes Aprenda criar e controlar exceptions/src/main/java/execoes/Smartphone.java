148cc3bca064c0445cec04412d003cc5704f4725
Matheus Biasi <biasimatheushenrique40@gmail.com>
2022-09-23 17:58:22 -0300

    Adicionando mudancas em master

--- Java Excecoes Aprenda criar e controlar exceptions/src/main/java/execoes/Smartphone.java
+++ Java Excecoes Aprenda criar e controlar exceptions/src/main/java/execoes/Smartphone.java
@@ -1,20 +1,34 @@
 package execoes;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "smartphones")
 public class Smartphone extends EntityAbstract{
-    private Long IdSmartphone;
+    @Id
+    @SequenceGenerator(name = "Smartphone_Generator",sequenceName = "idSmartphone",allocationSize = 1)
+    @GeneratedValue(generator = "Smartphone_Generator",strategy = GenerationType.SEQUENCE)
+	private Long IdSmartphone;
     private String chapa;
+    @ManyToOne
     private Modelo modelo;
+    @ManyToOne
     private Status Status;
+    @OneToOne
     private Usuario usuario;
+        
     
-    
-    
-    
-    
-	public String getModelo() {
+	public Modelo getModelo() {
 		return modelo;
 	}
-	public void setModelo(String modelo) {
+	public void setModelo(Modelo modelo) {
 		this.modelo = modelo;
 	}
 	public String getChapa() {
