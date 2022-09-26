582d39e6a55bdb86095fb9bc1740e31f3391e986
Matheus Biasi <biasimatheushenrique40@gmail.com>
2022-09-26 08:26:57 -0300

    Alterando projeto

--- Spring Boot API REST - Contrua uma API/forum/src/test/java/br/com/cooperalfa/forum/ForumApplicationTests.java
+++ Spring Boot API REST - Contrua uma API/forum/src/test/java/br/com/cooperalfa/forum/ForumApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package br.com.cooperalfa.forum;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class ForumApplicationTests {
 
 	@Test
 	void contextLoads() {
